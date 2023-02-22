aa999036a81abd14e0b960b8acc56760a2a01675
Takeyuki UEDA <ueda@latelierdueda.com>
2023-02-22 21:19:38 +0900

    add example

---
+++ example/vue2/cdn/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <QRcode/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+const QRcode = window.QRcode.QRcode
+
+export default {
+  name: 'App',
+  components: {
+    QRcode
+  }
+}
+</script>
