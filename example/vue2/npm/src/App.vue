5c251b74655e604d46584e6cff0495d2dd150803
Takeyuki UEDA <ueda@latelierdueda.com>
2023-02-22 20:40:23 +0900

    add example

---
+++ example/vue2/npm/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <QRcode/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import {QRcode} from 'qrcode-of-this-site2'
+
+export default {
+  components: {
+    QRcode,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
